6395c9a691db4ff13a836ccea1ddb6385771a5c7
softadastra <gaspardkirira9@gmail.com>
2025-10-05 19:49:34 +0300

    feat(server): fully non-blocking architecture with async stop and thread pool support
    - run() now starts async accept and IO threads without blocking the main thread
    - stop_async() cleanly shuts down acceptor and io_context_
    - Requests are handled via ThreadPool::enqueue(), keeping IO threads free
    - start_accept() is recursive and continues until stop_requested_ is true
    - Added monitor_metrics() for periodic display of ThreadPool state

--- app/App.cpp
+++ app/App.cpp
@@ -1,10 +1,5 @@
 #include "App.hpp"
 #include "../utils/Logger.hpp"
-#include <csignal>
-#include <thread>
-#include <atomic>
-#include <condition_variable>
-#include <mutex>
 
 namespace Vix
 {
@@ -17,37 +12,20 @@ namespace Vix
     {
         auto &log = Logger::getInstance();
         log.log(Logger::Level::INFO, "Received SIGINT, shutting down...");
-
         stop_flag = true;
         stop_cv.notify_one();
-
         if (g_server_ptr)
-        {
             g_server_ptr->stop_async();
-        }
     }
 
-    App::App()
-        : config_(Config::getInstance()),
-          server_(config_)
+    App::App() : config_(Config::getInstance()), server_(config_)
     {
         auto &log = Logger::getInstance();
         log.setLevel(Logger::Level::WARN);
-
-        try
-        {
-            config_.loadConfig();
-            router_ = server_.getRouter();
-
-            if (!router_)
-            {
-                log.throwError("Failed to get router from HTTPServer");
-            }
-        }
-        catch (const std::exception &e)
-        {
-            log.throwError("Failed to initialize App: {}", e.what());
-        }
+        config_.loadConfig();
+        router_ = server_.getRouter();
+        if (!router_)
+            log.throwError("Failed to get router from HTTPServer");
     }
 
     void App::run(int port)
@@ -69,7 +47,6 @@ namespace Vix
 
         if (server_thread.joinable())
             server_thread.join();
-
         server_.join_threads();
 
         log.log(Logger::Level::INFO, "Application shutdown complete");
